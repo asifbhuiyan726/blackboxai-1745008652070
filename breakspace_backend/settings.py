f12b38089dd04beb0fd1918395efeed7431f9426
Asif Bhuiyan <65206835+asifbhuiyan726@users.noreply.github.com>
2025-04-19 02:59:28 +0300

    Update settings.py

--- breakspace_backend/settings.py
+++ breakspace_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hjyvdge^ia+0m*q@7$cz^m%_v7h%l%o*lxnv1ioiyf3#wiz!%i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]  # Adjust this to your railway app domain in production
 
