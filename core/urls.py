4f3d15029b415c5534b259852cf6204a183f2b65
Asif Bhuiyan <65206835+asifbhuiyan726@users.noreply.github.com>
2025-04-19 00:18:04 +0300

    Add core/urls.py 1745011084215

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('breakrooms/', views.breakrooms, name='breakrooms'),
+    path('chatroom/', views.chatroom, name='chatroom'),
+    path('events/', views.events, name='events'),
+    path('profile/', views.profile, name='profile'),
+    path('settings/', views.settings, name='settings'),
+]
